fda95d3eae76236e36a4f98f117146be619546e1
Muhammad Adnan <79781004+Adnan-Swati@users.noreply.github.com>
2021-09-29 09:30:51 -0700

    Add files via upload

---
+++ practiceof function.dart
@@ -0,0 +1,51 @@
+// void main() {
+// var a =[-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7];
+//   var a1 =List.of(a.reversed);
+//   print (a);
+//   a.removeWhere((e)=>e<3);
+//   print (a);
+//   a.removeWhere((e)=>e<2);
+//   print(a);
+//   a.removeRange(0,5);
+//   print (a);
+//   a.retainWhere((e)=>e>4);
+//   print (a);
+  
+//   var a ={ 1:'adnan',2:"zeeshan",3:'yasir'};
+//   print(a);
+//   print (a.keys);
+//   print (a.values);
+ 
+//   var z = [1:'apple',2:'banana',3:'grapes'];
+//    var b= z.length;
+//   print (b);
+  
+  
+//   }
+
+
+
+
+// void main(){
+ 
+//   sum(20,40);
+//   sum(20,40);
+//   sum(20,40);
+//   sum(20,40);
+//   sum(20,40);
+//   sum(20,40);
+//   sum(20,40);
+//   sum(20,40);
+// }
+// // void sum(){
+// //   int a = 20;
+// //   int b = 40;
+// //   int  c = a+b;
+// //   print ( 'The sum of $a and $b is $c');
+// // }
+
+// void sum(int a,int b){
+
+//   int  c = a+b;
+//   print ( 'The sum of $a and $b is $c');
+// }
